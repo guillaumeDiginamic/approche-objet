646251dea572b6c0aeb81cdc6735b594525eee67
guillaumeDiginamic <cguillaume@diginamic-formation.fr>
2023-11-03 10:50:09 +0100

    TP4

---
+++ src/utils/TestMethodStatic.java
@@ -0,0 +1,12 @@
+package utils;
+public class TestMethodStatic {
+    public static void main(String[] args) {
+        String chaine ="12";
+        int num1 = Integer.parseInt(chaine);
+        System.out.println("num1 = "+num1);
+        int a = 10;
+        int b = 9;
+        int numMax = Integer.max(a, b);
+        System.out.println("numMax = "+numMax);
+    }
+}
